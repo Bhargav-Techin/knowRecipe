48c6e29a8f50ec0f1f33261ad20ecdc9140a8e79
Bhargav-Techin <148316999+Bhargav-Techin@users.noreply.github.com>
2025-02-20 01:14:24 +0530

    updated code for handling env

--- KnowRecipe/src/main/java/Recipe/dev/KnowRecipe/KnowRecipeApplication.java
+++ KnowRecipe/src/main/java/Recipe/dev/KnowRecipe/KnowRecipeApplication.java
@@ -1,13 +1,22 @@
 package Recipe.dev.KnowRecipe;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KnowRecipeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KnowRecipeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Load .env file
+        Dotenv dotenv = Dotenv.load();
 
+        // Set system properties
+        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+        System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+        System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+        System.setProperty("FRONTEND_URL", dotenv.get("FRONTEND_URL"));
+
+        SpringApplication.run(KnowRecipeApplication.class, args);
+    }
 }
--- KnowRecipe/src/test/java/Recipe/dev/KnowRecipe/KnowRecipeApplicationTests.java
+++ KnowRecipe/src/test/java/Recipe/dev/KnowRecipe/KnowRecipeApplicationTests.java
@@ -2,7 +2,9 @@ package Recipe.dev.KnowRecipe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class KnowRecipeApplicationTests {
 
